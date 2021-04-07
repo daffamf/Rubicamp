0a7983507f45018b04c343df4d92cc3f690c7d41
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-04-08 00:23:37 +0700

    done

--- c.1/challenge1.js
+++ c.1/challenge1.js
@@ -1,4 +1,3 @@
-
 function sum() {
     let penjumlahan = 0;
     for (let i = 0; i < arguments.length; i++) {
@@ -6,7 +5,7 @@ function sum() {
     }
     console.log(penjumlahan)
   }
-  sum(1, 2, 7);//10
-  sum(1, 4);//5
-  sum(11);//11
-  sum(10, 3, 6, 7, 9);//35
+  sum(1, 2, 7); // output 10
+  sum(1, 4);// output 5
+  sum(11); //output 11
+  sum(10, 3, 6, 7, 9);  //output 35
