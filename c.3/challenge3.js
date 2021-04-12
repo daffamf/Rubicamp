2891e731684201251d2ed4be0787c7e5e9ea466f
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-04-12 08:39:08 +0700

    push

--- c.3/challenge3.js
+++ c.3/challenge3.js
@@ -1,4 +1,3 @@
-
 function romawi(n) {
     var angka = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var huruf = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
@@ -9,6 +8,7 @@ function romawi(n) {
             n -= angka[i];
         }
     }
+
     return hasil
 }
 
