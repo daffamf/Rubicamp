da64e35bcbacce7ea4ebf9ed2a0a8fd007e9e05f
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-04-15 12:41:40 +0700

    push

---
+++ challenge5.js
@@ -0,0 +1,22 @@
+function stingmanipulation(word) {
+    var kaki = word[0]
+    if (kaki === 'a')
+        return word
+    else if (kaki === 'i')
+        return word
+    else if (kaki === 'u')
+        return word
+    else if (kaki === 'e')
+        return word
+    else if (kaki === 'o')
+        return word
+    else {
+        return word.slice(1) + kaki + 'nyo'
+    }
+
+}
+
+
+
+console.log(stingmanipulation('ayam'));
+console.log(stingmanipulation('bebek'));
