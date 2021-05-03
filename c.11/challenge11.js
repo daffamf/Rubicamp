d42b33f480584a1a7ae698358645c2b6888ddf00
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-05-03 11:08:49 +0700

    c.11

---
+++ c.11/challenge11.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var data = fs.readFileSync('data.json', 'utf8')
+var qna = JSON.parse(data)
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: 'Tebakan:'
+});
+var i = 0;
+console.log('Selamat datang dipermainan tebak kata , silahkan jawab dengan benar')
+console.log('')
+console.log(qna[i].question)
+rl.prompt();
+rl.on('line', (line) => {
+
+    if (line.trim().toLowerCase() == qna[i].answer){ 
+        i++
+        console.log('Selamat anda benar!');
+        console.log('')
+        if (i != qna.length) {
+            console.log(qna[i].question)
+            rl.prompt
+        } else {
+            rl.close()
+        }
+    } else {
+        console.log('wkwk anda kurang beruntung')
+        console.log('')
+        console.log(qna[i].question)
+        rl.prompt()
+    }
+}). on('close', () => {
+    console.log('Hore Anda Menang!');
+    process.exit(0);
+
+})
+
+
+
+
+
+
