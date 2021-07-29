6a84c030e3f80232e898329eaf52117bbc1a7282
daffamf <daffamuhammadfirdaus5@gmail.com>
2021-07-29 11:38:00 +0700

    inisial commit

--- c.2/challenge2.js
+++ c.2/challenge2.js
@@ -1,7 +1,7 @@
 function deretKaskus(n){
     var arr = []
     for(let i = 1; i <= n ; i++){
-        var hasil = i * 3 
+        var hasil = i * 3
         if((hasil % 5 == 0) && (hasil % 6 == 0))
         arr.push('KASKUS')
         else if(hasil % 5 == 0)
--- c.7/challenge7.js
+++ c.7/challenge7.js
@@ -3,15 +3,15 @@ function weirdMultiply(sentence) {
     var realAngka = angka.map(Number)
     if (realAngka.length > 1) {
         var arr = 1
-        for (let i = 0; i < realAngka.length; i++) {
+        for(let i = 0; i < realAngka.length; i++){
             arr = arr * realAngka[i]
         }
         return weirdMultiply(arr)
-    } else {
+    }else {
         return sentence
     }
 }
-console.log(weirdMultiply(39)); // output 4
-console.log(weirdMultiply(999)); // output 2
-console.log(weirdMultiply(3));  // output 3
 
+console.log(weirdMultiply(39))
+console.log(weirdMultiply(999))
+console.log(weirdMultiply(3))
